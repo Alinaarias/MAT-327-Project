aaebce7cf5dc1c3edd58a8626ca68633dafd985c
Alinaarias <alina.arias@lc.cuny.edu>
2024-10-14 22:48:42 -0400

    Add files via upload

---
+++ Milestone 5.R
@@ -0,0 +1,21 @@
+# Milestone 5
+
+mean(Hot$Danceability)
+median(Hot$Danceability)
+var(Hot$Danceability)
+sd(Hot$Danceability)
+
+mean(Hot$Duration)
+median(Hot$Duration)
+var(Hot$Duration)
+sd(Hot$Duration)
+
+mean(Hot$Tempo)
+median(Hot$Tempo)
+var(Hot$Tempo)
+sd(Hot$Tempo)
+
+mean(Hot$Popularity)
+median(Hot$Popularity)
+var(Hot$Popularity)
+sd(Hot$Popularity)
